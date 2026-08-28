752ed7fa3f970d3a014b334f3e23d1570be5d0e6
Karmugilan <karmugilanrama@gmail.com>
2026-08-28 10:04:00 +0530

    Add swap two numbers program

---
+++ BasicPrograms/SwapTwoNumbers.java
@@ -0,0 +1,25 @@
+package BasicPrograms;
+import java.util.Scanner;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first Number ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second Number ");
+        int b = sc.nextInt();
+
+        int temp = a;
+         a = b;
+         b = temp;
+
+        System.out.println("After Swapping");
+        System.out.println(" a = " + a);
+        System.out.println(" b = " + b);
+
+        sc.close();
+
+
+    }
+}
