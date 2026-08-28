42061952a6a36bf8178d4569b86acea0cbe0bd2b
Karmugilan <karmugilanrama@gmail.com>
2026-08-28 14:29:43 +0530

    Find Largest of Three Numbers

---
+++ BasicPrograms/LargestOfThree.java
@@ -0,0 +1,23 @@
+package BasicPrograms;
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        if( a >= b && a >= c) {
+            System.out.println("Largest Number is " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Largest Number is " + b);
+        } else {
+            System.out.println("Largest Number is " + c);
+        }
+
+        sc.close();
+    }
+    
+}
