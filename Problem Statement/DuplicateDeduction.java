a7702a34de654fcaff02cb717b1d77f3545ce850
Karmugilan <karmugilanrama@gmail.com>
2026-09-24 14:28:46 +0530

    Print Only Dupicate Elements

---
+++ Problem Statement/DuplicateDeduction.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class DuplicateDeduction {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+
+        }
+    }
+}
