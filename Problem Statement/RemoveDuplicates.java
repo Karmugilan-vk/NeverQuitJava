7e3ee4923f3a9fe4cbeca74d5f07b4456892385c
Karmugilan <karmugilanrama@gmail.com>
2026-09-24 13:42:21 +0530

    Remove the Dulicate Value

---
+++ Problem Statement/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static void main(String[] args){
+         Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+    }
+      for (int i = 0; i < n; i++) {
+         boolean Duplicate = false;
+        for(int j = 0; j < i; j++){
+            if(arr[i] == arr[j]){
+                Duplicate = true;
+            }
+
+        }
+        if(!Duplicate){
+        System.out.print(arr[i] + " ");
+        }
+}
+    }
+}
